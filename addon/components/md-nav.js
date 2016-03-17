b360ab8a55ef7a7248cfa8a6338188ba6f389b0e
Mike North <michael.l.north@gmail.com>
2016-03-17 10:51:45 +0800

    Sidenav connected to navbar

--- addon/components/md-nav.js
+++ addon/components/md-nav.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 import layout from '../templates/components/md-nav';
 
-export default Ember.Component.extend({
+const { run: { scheduleOnce }, Component } = Ember;
+
+export default Component.extend({
   classNames: ['md-nav'],
   layout,
-  tagName: 'nav'
+  tagName: 'nav',
+  didInsertElement() {
+    this._super(...arguments);
+    scheduleOnce('afterRender', () => {
+      this.$('.button-collapse').sideNav();
+    })
+  }
 });
--- addon/components/md-sidenav.js
+++ addon/components/md-sidenav.js
@@ -4,5 +4,11 @@ import layout from '../templates/components/md-sidenav';
 export default Ember.Component.extend({
   layout,
   classNames: ['side-nav'],
-  tagName: 'ul'
+  tagName: 'ul',
+  init() {
+    this._super(...arguments);
+    if (this.get('_parentNav')) {
+      this.set('_parentNav._sidenavId', this.get('elementId'));
+    }
+  }
 });
