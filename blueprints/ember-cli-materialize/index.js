a4803a598ce021db4ae3bfb13e71c2294f61dbe6
Mike North <michael.l.north@gmail.com>
2016-03-18 11:33:44 +0800

    installation blueprint fix

--- blueprints/ember-cli-materialize/index.js
+++ blueprints/ember-cli-materialize/index.js
@@ -6,13 +6,13 @@ module.exports = {
   description: 'Installation blueprint for ember-cli-materialize v1.x',
   normalizeEntityName: function() {},
   afterInstall: function(options) {
-    return this.addAddonsToProject([
+    return this.addAddonsToProject({packages: [
       {name: 'ember-materialize-shim', target: '~0.0.1'},
       {name: 'ember-truth-helpers', target: '^1.2.0'},
       {name: 'ember-composable-helpers', target: '~0.19.0'},
       {name: 'ember-cli-flash', target: '^1.3.14'},
       {name: 'ember-gestures',  target:'~0.4.1'},
       {name: 'ember-modal-dialog', target: '~0.8.3'}
-    ]);
+    ]});
   }
 };
